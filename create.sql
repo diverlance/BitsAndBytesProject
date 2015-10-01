e3aaa104cb8048d0c8da16ffd64ca2c50d747c8e
John Nguyen <mrjnguyen16@gmail.com>
2015-09-30 19:07:20 -0700

    joined tables containing class schedules. HW3

--- create.sql
+++ create.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.26, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.6.19, for osx10.7 (x86_64)
 --
--- Host: localhost    Database: Team
+-- Host: localhost    Database: hw1
 -- ------------------------------------------------------
--- Server version	5.6.26
+-- Server version	5.6.19
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -42,6 +42,52 @@ LOCK TABLES `members` WRITE;
 INSERT INTO `members` VALUES ('John','Nguyen','Male','Computer Science','john.nguyen15@sjsu.edu',4082022964),('Haoxuan','Dong','Male','Computer Science','Donghaoxuan@gmail.com',4084208911),('Lance','Roberts','Male','Computer Science','lance.roberts@sjsu.edu',4087754267),('CheungFaat','Chiu','Male','Computer Science','cheungfaat.chiu@sjsu.edu',9253251389);
 /*!40000 ALTER TABLE `members` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `records`
+--
+
+DROP TABLE IF EXISTS `records`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `records` (
+  `id` int(11) DEFAULT NULL,
+  `name` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `records`
+--
+
+LOCK TABLES `records` WRITE;
+/*!40000 ALTER TABLE `records` DISABLE KEYS */;
+INSERT INTO `records` VALUES (1,'John Nguyen'),(2,'Haoxuan Dong'),(3,'Lance Roberts'),(4,'CheungFaat Chiu');
+/*!40000 ALTER TABLE `records` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `schedule`
+--
+
+DROP TABLE IF EXISTS `schedule`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schedule` (
+  `id` int(11) DEFAULT NULL,
+  `Courses` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `schedule`
+--
+
+LOCK TABLES `schedule` WRITE;
+/*!40000 ALTER TABLE `schedule` DISABLE KEYS */;
+INSERT INTO `schedule` VALUES (1,'CS 160: Software Engineering'),(1,'CS 174: Web Programming'),(1,'CS 166: Information Security'),(2,'CS 174: Web Programming'),(2,'CS 153: Compiler Design'),(3,'CS 174: Web Programming'),(3,'CS 153: Compiler Design'),(3,'CS 157A: Intro to Databases'),(3,'CS 160: Software Engineering'),(3,'CS 116A: Intro to Computer Graphics'),(4,'CS 157A: Intro to Databases'),(4,'CS 108: Intro to Game Studies'),(4,'CS 160: Software Engineering'),(4,'CS 174: Web Programming'),(4,'CS 153: Compiler Design'),(3,'CS 152: Programming Paradigms');
+/*!40000 ALTER TABLE `schedule` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -52,4 +98,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-09-08 18:25:44
+-- Dump completed on 2015-09-30 18:52:02
---
+++ join.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>Team Members</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+    
+    <p>
+        <?php
+            $first = filter_input(INPUT_GET, "firstName");
+            $last  = filter_input(INPUT_GET, "lastName");
+            try
+            {
+                $con = new PDO("mysql:host=127.0.0.1;dbname=hw1", "USERNAME", "PASSWORD");
+                $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                if ((strlen($first) > 0) && (strlen($last) > 0))
+                {
+                    $query = $con->prepare("SELECT Courses FROM schedule, records ".
+                            " WHERE records.name = :name ".
+                            "AND records.id = schedule.id ");
+                    $query->execute(array(':name' => "$first $last"));
+                    $data = $query->fetchAll(PDO::FETCH_ASSOC);
+                }
+                print "<h1 class='title'>$first $last</h1>";
+                print "<table border='1'>\n";
+                $doHeader = true;
+                foreach ($data as $row)
+                {
+                    if ($doHeader)
+                    {
+                        print "        <tr>\n";
+                        foreach ($row as $name => $value)
+                        {
+                            print "            <th>$name</th>\n";
+                        }
+                        print "        </tr>\n";
+
+                        
+                        $doHeader = false;
+                    }
+                    print "            <tr\n>";
+                    foreach ($row as $name => $value)
+                    {
+                          print "                <td>$value</td>\n";
+                    }
+                    print "            </tr\n>";
+                    
+                }
+                print "        </table>\n";
+            }
+            catch(PDOException $ex)
+            {
+                echo 'ERROR: '.$ex->getMessage();
+            }
+        ?>
+    </p>
+</body>
+</html>
